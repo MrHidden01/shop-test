20263b641bd49da2589a62c32bb51e1b18e1cea3
MrHidden01 <52502595+MrHidden01@users.noreply.github.com>
2019-07-04 15:36:22 +0430

    Update cart.php

--- frontend/cart.php
+++ frontend/cart.php
@@ -86,44 +86,7 @@ $ids='';
                 echo Html::end_div();
                 echo Html::end_div();
                 echo Html::end_div();
-
-                echo Html::start_div('col-xs-12 col-lg-3 pull-right');
-                echo Html::start_div('widgetbox');
-                echo Html::start_div('headarea');
-                echo Html::h4('دسته ها');
-                echo Html::end_div();
-                echo Html::start_div('contentarea');
-
-                $product = new Category();
-                $model = $product->select()->from(Category::table_name)->orderBy('cat_id desc')->asArray()->all();
-
-                foreach ($model as $cat) {
-
-                    // var_dump($rows);
-                    echo Html::start_div('col-xs-12 col-sm-4');
-                    echo Html::p(Html::a("ProductByCategory.php?cat_id=$cat[cat_id]", $cat['cat_title']));
-                    echo Html::br();
-                    echo Html::end_div();
-
-                }
-                echo Html::end_div();
-                echo Html::end_div();
-                echo Html::end_div();
-                ?>
-    <div class="col-xs-12 col-lg-3 pull-right">
-
-        <div class="widgetbox">
-            <div class="headarea">
-                <h4>درباره ما</h4></div>
-            <div class="contentarea">
-                <p>
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-                    چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم ی دستاوردهای اصلی و جوابگوی
-                    سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-                </p>
-            </div>
-        </div>
-    </div>
-</div>
 <?php
-include_once 'template/footer.php';
+      include_once 'template/widget.php';
+
+      include_once 'template/footer.php';
