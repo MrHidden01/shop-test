ecb302fdf73600d85e36ba00b395a3296fc8b2ae
MrHidden01 <52502595+MrHidden01@users.noreply.github.com>
2019-07-04 15:08:20 +0430

    Update head.php

--- frontend/template/head.php
+++ frontend/template/head.php
@@ -27,16 +27,7 @@
     <![endif]-->
 </head>
 <a  class="btn-danger" style="border-radius: 3px; float: left; margin-top: 10px;margin-left: 10px" href="<?=Url::createFrontUrl('Register/logout.php')?>">خروج</a>
-<?php
-echo Html::start_div('head','style="border-radius: 3px; float: left; margin-top: 10px;margin-left: 10px"');
-echo Html::space();
-echo Html::a(Url::createFrontUrl('Register/register.php'),'ثبت نام');
-echo Html::space();
-echo '<span style="color: #2aabd2">|</span>';
-echo Html::space();
-echo Html::a(Url::createFrontUrl('Register/login.php'),'ورود');
-echo Html::end_div();
-?>
+
 <body>
 <div class="container">
     <div class="row">
