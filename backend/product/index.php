74bf70853903e3eb869e9313b70f870c60c3b003
MrHidden01 <52502595+MrHidden01@users.noreply.github.com>
2019-07-06 13:47:22 +0430

    Update index.php

--- backend/product/index.php
+++ backend/product/index.php
@@ -1,52 +1,63 @@
-
 <?php
 require_once "../../config.php";
 include '../Template/header.php';
+        $model= new  Product();
+        $query= $model->select()->leftJoin(["categories", "categories.cat_id", "products.product_category"])->all();
+foreach ($query as $ids) {
+    $id = $ids['product_id'];
+        $actions = "<a onclick='return confirm(\"آیا برای حذف مطمئن هستید ؟\")'class='btn btn-danger'href='" . Url::createBackendUrl("product/delete.php?product_id=" . $id) . "'
+            onclick>";
+        $actions .= "حذف</a> | ";
+        $actions .= "<a class='btn btn-info' href='" . Url::createBackendUrl("product/update.php?product_id=" . $id) . "'>";
+        $actions .= "ویرایش</a>";
+}
+
 echo Html::startCenter();
-
-//$cat = new Category();
-//$cat->select('cat_title')->from('categories,products')->where('product.product_category=categories.id');
-
-
-$columns=
+$columns =
     [
 
-        'product_name'=>[
-            'index'=>'product_name',
-            'header'=>'نام محصول',
-            'type'=>'text'
+        'product_id' => [
+            'index' => 'product_id',
+            'header' => 'ایدی محصول',
+            'type' => 'text'
+        ], 'product_name' => [
+        'index' => 'product_name',
+        'header' => 'نام محصول',
+        'type' => 'text'
+    ],
+        'product_price' => [
+            'index' => 'product_price',
+            'header' => 'قیمت محصول',
+            'type' => 'text'
         ],
-        'product_price'=>[
-            'index'=>'product_price',
-            'header'=>'قیمت محصول',
-            'type'=>'text'
+        'product_desc' => [
+            'index' => 'product_desc',
+            'header' => 'توضیحات محصول',
+            'type' => 'text'
         ],
-        'product_desc'=>[
-            'index'=>'product_desc',
-            'header'=>'توضیحات محصول',
-            'type'=>'text'
+        'product_image' => [
+            'index' => 'product_image',
+            'header' => 'عکس محصول',
+            'type' => 'image'
         ],
-        'product_image'=>[
-            'index'=>'product_image',
-            'header'=>'عکس محصول',
-            'type'=>'image'
-        ],
-        'product_category'=>[
-            'index'=>'cat_title',
-            'header'=>'دسته محصول',
-            'type'=>'text'
+        'product_category' => [
+            'index' => 'cat_title',
+            'header' => 'دسته محصول',
+            'type' => 'text'
         ]
     ];
-$grid=new Grid();
+
+$grid = new Grid();
 $grid->
 setTable(product::table_name)
     ->setColumens($columns)
     ->setUrl('product/')
-    ->show();
+    ->setAction($actions)
+    ->show($query);
 
-echo Html::a(Url::createBackendUrl('product/insert.php'),"اضافه کردن محصول جدید","class='btn btn-info'");
+echo Html::a(Url::createBackendUrl('product/insert.php'), "اضافه کردن محصول جدید", "class='btn btn-info'");
 echo Html::br();
 echo Html::br();
-echo Html::a(Url::createBackendUrl(''),"بازگشت","class='btn btn-danger'");
+echo Html::a(Url::createBackendUrl(''), "بازگشت", "class='btn btn-danger'");
 echo Session::getMessage();
 echo Html::endCenter();
